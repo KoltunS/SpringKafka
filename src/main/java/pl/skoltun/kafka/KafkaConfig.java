a3bb382798c86706a108de377253d3f03ae1f901
skoltun <sebastian.koltun7@gmail.com>
2019-05-20 23:40:17 +0200

    Kafka Spring Docker added properties

--- src/main/java/pl/skoltun/kafka/KafkaConfig.java
+++ src/main/java/pl/skoltun/kafka/KafkaConfig.java
@@ -1,12 +1,16 @@
 package pl.skoltun.kafka;
 
-import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -17,13 +21,16 @@ import java.util.Map;
 
 @Configuration
 @EnableKafka
+@PropertySource("classpath:spring.properties")
 public class KafkaConfig {
+    @Value("${kafka.bootstrap.servers}")
+    private String bootstrapServers;
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<String, String>
-                        kafkaListenerContainerFactory() {
+    kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                                new ConcurrentKafkaListenerContainerFactory<>();
+                new ConcurrentKafkaListenerContainerFactory<>();
         DefaultKafkaConsumerFactory<String, String> consumerFactory =
                 new DefaultKafkaConsumerFactory<>(consumerProperties());
         factory.setConsumerFactory(consumerFactory);
@@ -42,14 +49,14 @@ public class KafkaConfig {
         return new Listener();
     }
 
-    private Map<String, Object> consumerProperties(){
-        return Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092",
+    private Map<String, Object> consumerProperties() {
+        return Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName(),
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     }
 
-    private Map<String, Object> producerProperties(){
-        return Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092",
+    private Map<String, Object> producerProperties() {
+        return Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(),
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     }
